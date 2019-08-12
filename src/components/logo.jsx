74d665f7ef86ddfabe735db4b39f00556b75d36a
DmitriyS87 <schukindmitriy@gmail.com>
2019-08-12 16:51:25 +0800

    changes Logo styles

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledContainer = styled.div`
-  display: block;
-  height: 48px;
-  width: 209.25px;
+  display: inline-block;
+  height: 24px;
+  width: 105px;
+  margin-right: 10px;
+
+  @media (min-width: 600px) {
+    height: 48px;
+    width: 209.25px;
+    margin-right: 30px;
+  }
 `;
 
 const StyledImg = styled.img`
-    width: 100%;
-    height: auto;
+  width: 100%;
+  height: auto;
 `;
-
 
 const Logo = () => {
   return (
@@ -19,6 +25,6 @@ const Logo = () => {
       <StyledImg src="./img/logo.png" alt="logo" />
     </StyledContainer>
   );
-}
+};
 
 export default Logo;
